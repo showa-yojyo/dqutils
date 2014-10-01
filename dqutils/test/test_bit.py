316e8224327b2982a6d06be583f48f938fe5375b
showa_yojyo <yojyo@hotmail.com>
2014-10-01 21:43:12 +0900

    Add a test case to BitTestCase.

--- dqutils/test/test_bit.py
+++ dqutils/test/test_bit.py
@@ -10,24 +10,31 @@ from dqutils.bit import get_int
 class BitTestCase(unittest.TestCase):
     """Test functions defined in dqutils.bit."""
 
-    def setUp(self):
-        """Setup the test case."""
-        self.data = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06]
-
     def test_get_int(self):
         """Test function dqutils.bit.get_int."""
-        data = self.data
-        self.assertEqual(get_int(data, 0, 1), 0x00)
-        self.assertEqual(get_int(data, 1, 1), 0x01)
-        self.assertEqual(get_int(data, 0, 2), 0x0100)
-        self.assertEqual(get_int(data, 6, 2), 0x0006)
-        self.assertEqual(get_int(data, 0, 3), 0x020100)
-        self.assertEqual(get_int(data, 5, 3), 0x000605)
-        self.assertEqual(get_int(data, 6, 3), 0x000006)
+
+        targets = ((0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06),
+                   b'\x00\x01\x02\x03\x04\x05\x06',)
+
+        for data in targets:
+            self.assertEqual(get_int(data, 0, 1), 0x00)
+            self.assertEqual(get_int(data, 1, 1), 0x01)
+            self.assertEqual(get_int(data, 0, 2), 0x0100)
+            self.assertEqual(get_int(data, 6, 2), 0x0006)
+            self.assertEqual(get_int(data, 0, 3), 0x020100)
+            self.assertEqual(get_int(data, 5, 3), 0x000605)
+            self.assertEqual(get_int(data, 6, 3), 0x000006)
+
+    def test_get_int_empty(self):
+        """Test function dqutils.bit.get_int in case of empty value is
+        passed.
+        """
+        self.assertEqual(get_int(b'', 6, 3), 0)
+        self.assertEqual(get_int([], 6, 3), 0)
 
     def test_getbits(self):
         """Test function dqutils.bit.getbits."""
-        data = self.data
+        data = (0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06)
         self.assertEqual(getbits(data, 0, 0xFF00), 0x0001)
         self.assertEqual(getbits(data, 1, 0xFFFF), 0x0201)
         self.assertEqual(getbits(data, 0, 0x0100), 0x0001)
