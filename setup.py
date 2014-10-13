3f8a51ee72f1ea3d0e616ccde6407eb2dbdf1c19
showa_yojyo <yojyo@hotmail.com>
2014-10-13 22:55:45 +0900

    Rewrite setup.py and MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,17 +1,63 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
-"""setup script for dqutils package"""
+"""Setup script for dqutils (Dragon Quest Utilities) package."""
 
-from distutils.core import setup
-setup(name='dqutils',
-      version='1.1.0',
-      description='dqutils (Python version)',
-      author='yojyo@hotmail.com',
-      author_email='yojyo@hotmail.com',
-      url='http://www.geocities.jp/showa_yojyo/dq/',
-      packages=['dqutils',
-                'dqutils.dq3',
-                'dqutils.dq5',
-                'dqutils.dq6',
-                'dqutils.test'],
-      package_data={'dqutils.test':['conf/*.conf', 'conf/*.conf.error']})
+from setuptools import setup, find_packages
+
+setup(
+    name='dqutils',
+
+    # Versions should comply with PEP440.
+    version='1.1.0.dev1',
+
+    description='dqutils (Dragon Quest Utilities)',
+    #long_description=description,
+
+    # The project's main homepage.
+    url='https://github.com/showa-yojyo/dqutils',
+
+    # Author details.
+    author='プレハブ小屋',
+    author_email='yojyo@hotmail.com',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for.
+        'Intended Audience :: Other Audience',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',],
+
+    # What does your project relate to?
+    keywords='DRAGON QUEST SNES',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # Use MANIFEST.in during install where we specify additional files.
+    include_package_data=True,
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'dqutils': ['*.xml', 'sample/*.xml'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[('sample', ['sample/+.*.xml'])],
+    )
